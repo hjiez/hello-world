754cc2a77ac53950396922643ff47dddbec2d15b
zhou-jie-cloud <80158649+zhou-jie-cloud@users.noreply.github.com>
2022-12-28 16:29:11 +0800

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,18 @@
+#include<algorithm>
+#include<string.h>
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#define ll long long
+using namespace std;
+#include<math.h>
+#define INF 0x3f3f3f3f
+
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define maxn 1010
+ll a[100];
+int main()
+{
+	ll b[12];
+	cout<<b[0]; 
+}
